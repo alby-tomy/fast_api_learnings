95f9f4ec142b321af75f022d99f428f14beb2af1
alby-tomy <alby.u.tomy@gmail.com>
2026-04-20 12:16:18 +0530

    assignment project 2

--- fast_api_basic/Project1_FastAPI_Resuest_Method_Logic_Books_Project/project2/books.py
+++ fast_api_basic/Project1_FastAPI_Resuest_Method_Logic_Books_Project/project2/books.py
@@ -10,13 +10,15 @@ class Book:
     author : str
     description : str
     rating : int
+    publish_date : int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, publish_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.publish_date = publish_date
 
 class BookRequest(BaseModel):
     # beofre id : Optional[int] = None
@@ -25,26 +27,29 @@ class BookRequest(BaseModel):
     author : str = Field(min_length=3, max_length=20)
     description : str = Field(min_length=10, max_length=100)
     rating : int = Field(ge=0, le=5)
-    
+    publish_date : int = Field(ge=1999, lt=2031)
+
     model_config = {
         "json_schema_extra":{
             "example": {
                 "title": "Computer Science",
                 "author": "condingwithroby",
                 "description": "This is a book about computer science",
-                "rating": 4
+                "rating": 4,
+                "publish_date": 2012
+
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, "Computer Science", "condingwithroby", "This is a book about computer science", 4),
-    Book(2, "Python Programming", "condingwithroby", "This is a book about python programming", 5),
-    Book(3, "Be Fast with FastAPI", "condingwithroby", "This is a book about FastAPI", 4),
-    Book(4, "Master Endpoints", "Author 1", "This is a book about master endpoints", 5),
-    Book(5, "Artificial Intelligence", "Author 2", "This is a book about artificial intelligence", 4),
-    Book(6, "Data Science", "Author 3", "This is a book about data science", 5),
+    Book(1, "Computer Science", "condingwithroby", "This is a book about computer science", 4, 2012),
+    Book(2, "Python Programming", "condingwithroby", "This is a book about python programming", 5, 2013),
+    Book(3, "Be Fast with FastAPI", "condingwithroby", "This is a book about FastAPI", 4, 2012),
+    Book(4, "Master Endpoints", "Author 1", "This is a book about master endpoints", 5, 2015),
+    Book(5, "Artificial Intelligence", "Author 2", "This is a book about artificial intelligence", 4, 2016),
+    Book(6, "Data Science", "Author 3", "This is a book about data science", 5, 2012),
 ]
 
 
@@ -82,3 +87,26 @@ async def update_book(updated_book: BookRequest):
         if BOOKS[i].id == updated_book.id:
             BOOKS[i] = Book(**updated_book.model_dump())
             return BOOKS[i]
+        
+
+@app.delete("/books/delete-book/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+
+'''
+Assignment
+
+Here is your opportunity to keep learning!
+Add a new field to Book and BookRequest called published_date: int (for example, published_date: int = 2012).So, this book as published on the year of 2012.
+Enhance each Book to now have a published_date
+Then create a new GET Request method to filter by published_date
+'''
+
+@app.get("/books/published_date/{published_date}")
+async def read_book_by_published_date(published_date:int):
+    books_by_published_date = [book for book in BOOKS if book.publish_date == published_date]
+    return books_by_published_date
