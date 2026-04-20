ffacb04d1cd728d238d4c22aedb08da87b8f3748
alby-tomy <alby.u.tomy@gmail.com>
2026-04-21 03:37:11 +0530

    getting data from DB

--- fast_api_basic/Projects_FastAPI/ToDoApp/main.py
+++ fast_api_basic/Projects_FastAPI/ToDoApp/main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
-from database import engine
+from database import engine, SessionLocal
 
 app = FastAPI()
 
-
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/",status_code=status.HTTP_200_OK)
+async def read_all(db:db_dependency):
+    return db.query(models.Todos).all()
+
+@app.get("/todo/getById/{todo_id}",status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependency, todo_id:int = Path(gt=0)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code = 404, detail='Todo not found')
+
