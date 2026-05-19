56ea762b8f13d41a5887441244a4b9b1a564e225
alby-tomy <alby.u.tomy@gmail.com>
2026-05-20 04:03:27 +0530

    learning alembic migration

--- fast_api_basic/Projects_FastAPI/ToDoApp/models.py
+++ fast_api_basic/Projects_FastAPI/ToDoApp/models.py
@@ -12,6 +12,7 @@ class Users(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     role = Column(String)
+    # phone_number =Column(String, unique=True) # to test alembic migration
 
 class Todos(Base):
     __tablename__ = 'todos' #table name
