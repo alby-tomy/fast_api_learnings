4b7db4e32db1691dc4ea17929b534494c81b9b90
alby-tomy <alby.u.tomy@gmail.com>
2026-06-29 05:55:47 +0530

    added test cases

--- fast_api_basic/Projects_FastAPI/ToDoApp/database.py
+++ fast_api_basic/Projects_FastAPI/ToDoApp/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 """
 SQLite database configuration module for SQLAlchemy ORM.
@@ -19,8 +18,11 @@ Note:
     when different threads try to access the database through pooled connections.
 """
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root123@localhost/ToDoApplicationDB'
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- fast_api_basic/Projects_FastAPI/ToDoApp/main.py
+++ fast_api_basic/Projects_FastAPI/ToDoApp/main.py
@@ -8,6 +8,10 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+@app.get("/healthy")
+def heal_check():
+    return {"message": "API is healthy"}
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
